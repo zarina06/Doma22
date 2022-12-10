9a26eae6d6b1babd78b1afc11c7015cf753dc1c0
zarina <zarina100685@mail.ru>
2022-12-11 02:55:17 +0300

    Работа выполнена

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Домашняя работа 2.
+// Задача 1.Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+/*
+
+
+
+Console.WriteLine("Input a num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = (num/10) % 10;
+Console.WriteLine(num);
+*/
+//Задача 2.Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+
+//Найти третью цифру числа или сообщить, что ее нет 
+Console.WriteLine("Введите число");
+int number = 
+int.Parse(Console.ReadLine());
+string str = number.ToString();
+int index = 0;
+
+
+Console.WriteLine(str[2]);
+*/
+
+//Задача 3.Напишите программу, которая принимает на вход цифру, общначающую день недели , и проверяет , является ли этот день выходным.
+/*
+
+Console.WriteLine("Введите номер дня недели ");
+int a = int.Parse(Console.ReadLine());
+if (a > 5&& a <8 )
+{
+    Console.WriteLine("Выходной день" );
+}
+else if (a > 6)
+{
+  Console.WriteLine("Будний день" ) ;
+}
+if (a > 7)
+
+{
+Console.WriteLine("Ошибка");
+*/
+
+
+
+
+
